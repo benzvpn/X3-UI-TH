81981b223fc4ec92f532d01a784fa5d2fcefce47
benzvpn <53870629+benzvpn@users.noreply.github.com>
2025-12-26 20:01:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ yellow='\033[0;33m'
 plain='\033[0m'
 
 cur_dir=$(pwd)
-apt install iptables-persistent -y
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
 
